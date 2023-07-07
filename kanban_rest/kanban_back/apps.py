a7713540b9f0c550deaa1d93173d4bc4d8b834f0
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2023-07-06 21:10:16 -0300

    registro dos models seguindo novo formato de AppConfig

--- kanban_rest/kanban_back/apps.py
+++ kanban_rest/kanban_back/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class KanbanBackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'kanban_back'
+    def ready(self):
+        from .models import Tarefa, Coluna, Quadro
+        from django.contrib import admin
+        admin.site.register(Quadro)
+        admin.site.register(Coluna)
+        admin.site.register(Tarefa)
--- kanban_rest/kanban_back/models.py
+++ kanban_rest/kanban_back/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Quadro(models.Model):
+    nome = models.CharField(max_length=50)
+    #class Meta:
+    #    app_label = 'kanban_back'
+
+class Coluna(models.Model):
+    nome = models.CharField(max_length=50)
+    quadro = models.ForeignKey(Quadro, on_delete=models.CASCADE)
+    #class Meta:
+    #    app_label = 'kanban_back'
+
+class Tarefa(models.Model):
+    descricao = models.CharField(max_length=500)
+    coluna = models.ForeignKey(Coluna, on_delete=models.CASCADE)
+    #class Meta:
+    #    app_label = 'kanban_back'
